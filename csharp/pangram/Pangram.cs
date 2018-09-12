faf7f73ee963ef087cc917632e06895504c2c892
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2018-09-12 13:35:27 +0300

    add solution "Pangram"

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        HashSet<char> hashSet = new HashSet<char>();
+
+        foreach (char ch in input.ToLower())
+        {
+            if (Char.IsLetter(ch))
+            {
+                hashSet.Add(ch);
+            }
+        }
+
+        return (hashSet.Count == 26);
     }
 }
