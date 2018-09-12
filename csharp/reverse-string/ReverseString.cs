23b16805de50a2774a3e89ad1e08ba1af69ba6e7
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2018-09-12 11:47:00 +0300

    add solution for "Reverse String"

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        StringBuilder retString = new StringBuilder();
+
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            retString.Append(input.Substring(i, 1));
+        }
+
+        return retString.ToString();
+    }
+}
