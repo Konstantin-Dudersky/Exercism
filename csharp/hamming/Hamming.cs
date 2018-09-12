26775a5d884f8425805b054e3a2f6d0743fa5d1b
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2018-09-12 12:57:41 +0300

    add solution "Hamming"

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -4,6 +4,22 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException();
+        }
+
+        char[] firstArray = firstStrand.ToCharArray();
+        char[] secondArray = secondStrand.ToCharArray();
+        int count = 0;
+
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            if (firstArray[i] != secondArray[i])
+                count++;
+        }
+
+        return count;
+
     }
 }
