031a4e4253d6cc60bf15864187d33a15ae31b9bd
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2018-09-12 15:28:25 +0300

    add solution "Robot Name"

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,17 +1,58 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 public class Robot
 {
+    private string name = null;
+    private Random random = new Random();
+
+    private static List<string> usedNames;
+
+    public Robot()
+    {
+        name = null;
+    }
+    static Robot()
+    {
+        usedNames = new List<string>();
+    }
+
     public string Name
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (name == null)
+            {
+                while (true)
+                {
+                    char[] temp = new char[]{
+                        (char) ('A' + random.Next(0, 26)),
+                        (char) ('A' + random.Next(0, 26)),
+                        (char) ('0' + random.Next(0, 10)),
+                        (char) ('0' + random.Next(0, 10)),
+                        (char) ('0' + random.Next(0, 10)),
+                    };
+                    string tempStr = new String(temp);
+                    if (usedNames.Contains(tempStr))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        name = tempStr;
+                        usedNames.Add(name);
+                        break;
+                    }
+                }
+            }
+            return name;
         }
     }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        name = null;
     }
+
 }
