022f5148bd2a6cf14329a1c9ea34110cc65afb76
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2018-09-18 13:24:56 +0300

    add solution "Grains"

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Grains
 {
     public static ulong Square(int n)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return (n > 0 && n <= 64) ? (ulong)Math.Pow(2, n - 1) : throw new ArgumentOutOfRangeException();
     }
 
     public static ulong Total()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return (from s in Enumerable.Range(1, 64) select Square(s)).Aggregate((x, y) => x + y);
     }
+
 }
